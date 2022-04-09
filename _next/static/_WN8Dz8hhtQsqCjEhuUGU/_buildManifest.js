e1d76669bb8f12b11c047943c3586bf5b3d6dc7f
Lili00E <lili.even@gmx.de>
2022-04-09 16:22:05 +0200

    first version

---
+++ _next/static/_WN8Dz8hhtQsqCjEhuUGU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/545f34e4-c1135ceb05436843.js","static/chunks/170-28dc50d70350fe6f.js","static/chunks/pages/index-2a065e6ff6526e22.js"],"/_error":["static/chunks/pages/_error-427958541d1e32b7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
